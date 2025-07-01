735762933c40fe61ea6a8b8f0d58ab86f1e7b048
Santra32 <123738442+Santra32@users.noreply.github.com>
2025-07-01 12:27:49 +0530

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+if number > 10:
+    print("Your number is greater than 10.")
+elif number == 10:
+    print("Your number is exactly 10.")
+else:
+    print("Your number is less than 10.")
